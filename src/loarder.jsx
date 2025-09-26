c95fdee7e5509339b44e770b694fe3b8cec90eba
Blessing-pi <blessingowolabi23@gmail.com>
2025-09-26 05:52:47 -0700

    adjusted loader

---
+++ src/loarder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ScaleLoader} from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div>
+      <ScaleLoader color="#36D7B7" />
+    </div>
+  )
+}
+
+export default Loader
